6406308ab6afa7379a348aacef3079ea6f346ea6
Aditya <adityamane40@yahoo.in>
2026-05-05 13:48:13 +0530

    updated controller file

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,11 +1,14 @@
 package com.example.demo;
 
 import org.springframework.web.bind.annotation.*;
-@GetMapping("/fail")
-public String fail() {
-    throw new RuntimeException("Test error");
-}
 
+@RestController
+public class HelloController {
+
+    @GetMapping("/fail")
+    public String fail() {
+        throw new RuntimeException("Test error");
+    }
 @RestController
 public class HelloController {
 
